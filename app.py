295df017d712ec2327ba1708187dab2d443d13df
Felix Leber <email@felixleber.de>
2021-06-23 21:30:11 +0200

    Try if docker compose updates image

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def hello_tobi():
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, Version 2!'
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
