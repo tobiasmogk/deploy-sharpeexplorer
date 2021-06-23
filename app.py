2c670292099a1bc4b821f30faf2187224577e391
Felix Leber <email@felixleber.de>
2021-06-23 20:01:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask
 app = Flask(__name__)
 
+@app.route('/tobi')
+def hello_tobi():
+    return 'Hello, Tobi!'
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
+
